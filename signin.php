9d0dcfd1fb6f95cda489c4beddd28f29351fa21b
Debjit Roy <debjitroy515@gmail.com>
2023-10-28 21:18:42 +0530

    singn widtg

--- signin.php
+++ signin.php
@@ -4,14 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="mystyle.css">
     <title>Document</title>
 </head>
 <body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <main class="form-signin w-100 m-auto">
+
+
   <form>
     
-    <div class="p-3 mb-2 bg-info text-dark">.bg-info
+    <div class="p-3 mb-2 bg-info text-dark formcustom">.bg-info
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
       
           <div class="form-floating">
@@ -33,6 +36,7 @@
           <p class="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
         </div>
         </form>
+        
       </main>
 </body>
 </html>
