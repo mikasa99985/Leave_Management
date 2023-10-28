d75b4f1abb10db1e2e448d5691907177611c7f11
Akash <debnathakash911@gmail.com>
2023-10-29 00:11:40 +0530

    made some changes

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php require_once "_dbcon.php"?>
 <?php session_start(); ?>
 
-<?php
-echo "hello world!";
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -18,6 +15,7 @@ echo "hello world!";
 <body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
 </script>
+
 <header>
         <h1>Welcome to Our Website</h1>
     </header>
