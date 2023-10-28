e19e7a68a91aeec4a0830c11f028284f1129caee
Debjit Roy <debjitroy515@gmail.com>
2023-10-28 14:17:23 +0530

    1

--- signin.php
+++ signin.php
@@ -3,13 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    <main class="form-signin w-100 m-auto">
-        <form>
-          <img class="mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-          <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<main class="form-signin w-100 m-auto">
+  <form>
+    
+    <div class="p-3 mb-2 bg-info text-dark">.bg-info
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
       
           <div class="form-floating">
             <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
@@ -28,6 +31,7 @@
           </div>
           <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
           <p class="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
+        </div>
         </form>
       </main>
 </body>
