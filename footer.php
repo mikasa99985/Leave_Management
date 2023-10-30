9b2b8064294a63eee7548c4b801ded583600a6f3
Akash <debnathakash911@gmail.com>
2023-10-30 13:13:00 +0530

    make footer changes

--- footer.php
+++ footer.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Attractive Bootstrap Footer</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link href="footer.css" rel="stylesheet">
-    
-        
+
+
 </head>
+
 <body>
 
-<footer class="bg-dark text-white text-center custom-footer">
-    <div class="container">
+    <footer class="bg-dark text-white text-center custom-footer">
+
         <div class="row">
             <div class="col-md-4 text-left"> <!-- Added class 'text-left' -->
                 <h5>Contact Us</h5>
@@ -25,25 +27,27 @@
                 <a href="#" class="text-white">Twitter</a><br>
                 <a href="#" class="text-white">Instagram</a>
             </div>
-            <div class="col-md-4 text-right"> <!-- Added class 'text-right' -->
-                <h5>Subscribe</h5>
-                <form>
-                    <div class="form-group">
-                        <input type="email" class="form-control" placeholder="Enter your email">
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-block btn-sm">Subscribe</button>
-                </form>
-            </div>
+            <div class="col-md-3 ml-auto text-right"> <!-- Added class 'ml-auto' -->
+    <h5 class="text-center">Subscribe</h5>
+    <form>
+        <div class="form-group">
+            <input type="email" class="form-control" placeholder="Enter your email">
         </div>
-    </div>
-    <div class="container mt-2">
-        <p class="mb-0">&copy; 2023 Your Company</p>
-    </div>
-</footer>
+        <button type="submit" class="btn btn-primary btn-block btn-sm">Subscribe</button>
+    </form>
+</div>
+
+
+        </div>
+        <div class="container mt-2">
+            <p class="mb-0">&copy; 2023 Your Company</p>
+        </div>
+    </footer>
 
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
+
 </html>
